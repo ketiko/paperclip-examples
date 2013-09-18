620ecde3297f361c0ded200c2727da06b5b7994d
Ryan Hansen <ketiko@gmail.com>
2013-09-18 10:04:08 -0600

    Creating rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PaperclipExamples::Application.config.secret_key_base = '2e35b00f9b202171a88c7c737f9f5a66c604e45cb06410ecdb6b6c4ca0e0e5d0a56047fa969d6480329659f9ee340c4c0ed4d8cd97997ee344930ce316d78a6a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PaperclipExamples::Application.config.session_store :cookie_store, key: '_paperclip-examples_session'
